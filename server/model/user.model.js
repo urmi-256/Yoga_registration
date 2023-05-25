8426b7b11bec09bb6d36ea84555b11b48d74872f
Urmi Chauhan <urmi.chauhan05@gmail.com>
2023-05-25 19:20:00 +0530

    yoga_registration

--- server/model/user.model.js
+++ server/model/user.model.js
@@ -5,7 +5,6 @@ const User = new mongoose.Schema(
     name: { type: String, required: true },
     email: { type: String, required: true },
     password: { type: String, required: true },
-    dob: { type: String, required: true },
   },
   { collection: "user-data" }
 );
